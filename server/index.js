1232f24c6dcb39727a514cf0ee619a8f3e0baaeb
Александр <darkginesys@gmail.com>
2018-11-28 17:14:29 +0300

    delete status 404

--- server/index.js
+++ server/index.js
@@ -21,10 +21,6 @@ if (process.env.NODE_ENV === 'production') {
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
-app.all('/', async (req, res) => {
-    res.status(404).send();
-});
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port http://localhost:${port}/`));
