686d34860efc4ffec69760a2c560842c8ca6cc92
Александр <darkginesys@gmail.com>
2018-11-28 16:16:49 +0300

    Added static folder prep SPA

--- server/index.js
+++ server/index.js
@@ -9,11 +9,22 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const posts = require('./routes/api/posts');
+
 app.use('/api/posts', posts);
+
+// Hendle production
+if (process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(_dirname + '/public/'));
+
+    // Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 app.all('/', async (req, res) => {
     res.status(404).send();
 });
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server started on port http://localhost:${port}/`));
